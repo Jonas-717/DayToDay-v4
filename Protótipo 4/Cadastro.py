075a3ba0985fd4e32daab1cb7ee04f999884e7f9
Jonas <clajon854@gmail.com>
2025-12-10 02:01:21 -0300

    Cadastro.py

--- Protótipo 4/Cadastro.py
+++ Protótipo 4/Cadastro.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import re
+import os
+
+print("Existe?", os.path.exists(logo))
 
 # COR DE FUNDO DA PÁGINA.
 st.markdown("""
@@ -63,5 +66,6 @@ st.write("Motivo 3 para usar o DayToDay: Personalizar suas listas e rotinas de a
 st.write("Motivo 4 para usar o DayToDay: Acompanhar seu progresso e se manter motivado diariamente.")
 
 st.write("Motivo 5 para usar o DayToDay: Acessar suas tarefas de qualquer lugar, a qualquer momento.")
+
 
 
